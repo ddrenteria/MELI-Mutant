b6dc879b768e6aad8ea7e3a704d9450250e66322
Dago <dago.derenteria@wearesh.com>
2021-12-21 19:29:59 -0300

    feat: add validation for vertical rows with no test

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,25 +2,64 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHorizontalConsecutiveSequencesUntilTwo(dna: string[], consecutive_gen_sequences) {
+  aaaa(
+    dna: string[],
+    consecutive_gen_sequences: number,
+    last_gen: string,
+    count: number,
+    row: number,
+    column: number
+    ) {
+    if (dna[row][column] === last_gen || last_gen === '') {
+      count++;
+      // If we have a sequence of 4 elements increse consecutive gen sequences and reset count and last gen
+      if (count === 4) {
+        consecutive_gen_sequences++;
+        count = 0;
+        last_gen = '';
+      }
+    } else {
+      count = 1;
+    }
+    last_gen = dna[row][column];
+    return { consecutive_gen_sequences, last_gen, count };
+  }
+
+  getHorizontalConsecutiveSequencesUntilTwo(dna: string[], consecutive_gen_sequences: number) {
     const dnaRows = dna.length;
     const dnaCols = dna[0]?.length || 0;
     for (let i = 0; i < dnaRows && consecutive_gen_sequences <= 1; i++) {
       let count = 0;
       let last_gen = '';
       for (let j = 0; j < dnaCols && consecutive_gen_sequences <= 1; j++) {
-        if (dna[i][j] === last_gen || last_gen === '') {
-          count++;
-          // If we have a sequence of 4 elements increse consecutive gen sequences and reset count and last gen
-          if (count === 4) {
-            consecutive_gen_sequences++;
-            count = 0;
-            last_gen = '';
-          }
-        } else {
-          count = 1;
-        }
-        last_gen = dna[i][j];
+        ({ consecutive_gen_sequences, last_gen, count } = this.aaaa(
+          dna,
+          consecutive_gen_sequences,
+          last_gen,
+          count,
+          i,
+          j
+        ));
+      }
+    }
+    return consecutive_gen_sequences;
+  }
+
+  getVerticalConsecutiveSequencesUntilTwo(dna: string[], consecutive_gen_sequences) {
+    const dnaRows = dna.length;
+    const dnaCols = dna[0]?.length || 0;
+    for (let j = 0; j < dnaCols && consecutive_gen_sequences <= 1; j++) {
+      let count = 0;
+      let last_gen = '';
+      for (let i = 0; i < dnaRows && consecutive_gen_sequences <= 1; i++) {
+        ({ consecutive_gen_sequences, last_gen, count } = this.aaaa(
+          dna,
+          consecutive_gen_sequences,
+          last_gen,
+          count,
+          i,
+          j
+        ));
       }
     }
     return consecutive_gen_sequences;
@@ -32,7 +71,7 @@ export class AppService {
 
     // Check horizontal
     consecutive_gen_sequences = this.getHorizontalConsecutiveSequencesUntilTwo(dna, consecutive_gen_sequences);
-    
+    consecutive_gen_sequences = this.getVerticalConsecutiveSequencesUntilTwo(dna, consecutive_gen_sequences);
 
     if (consecutive_gen_sequences > 1) {
       isMutant = true;
