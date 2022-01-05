db8a45ef1296af926be9c49aa6a5373ca89d5ec6
Dago <dago.derenteria@wearesh.com>
2022-01-05 17:21:38 -0300

    fix: fix module typeorm

--- src/dna/dna.module.ts
+++ src/dna/dna.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DnaService } from './service/dna.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DnaController } from './controller/dna.controller';
+import { Dna } from './dna.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Dna])],
   controllers: [DnaController],
   providers: [DnaService]
 })
